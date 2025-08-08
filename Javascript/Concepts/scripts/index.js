91ecab20dc0e58ede5cb8217646cc5ca75867ed4
Dinesh Sripathi <dineshsri718@gmail.com>
2025-08-08 15:28:38 +0530

    added the furniStore and javascript workspace to the repo

---
+++ Javascript/Concepts/scripts/index.js
@@ -0,0 +1,10 @@
+let value1 = 6;
+let value2 = 7;
+
+//add value2 with value1 and store the result inside value1
+
+console.log(value1+" before the operation")
+
+value1 += value2; //value1 = value1 + value2
+
+console.log(value1+" after the operation")
