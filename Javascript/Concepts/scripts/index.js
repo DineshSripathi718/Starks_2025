6c7a5f3facd804d53ab0df033080021d61601b25
Dinesh Sripathi <dineshsri718@gmail.com>
2025-08-12 15:53:31 +0530

    completed operators upto Increment and decrement

--- Javascript/Concepts/scripts/index.js
+++ Javascript/Concepts/scripts/index.js
@@ -1,10 +1,47 @@
-let value1 = 6;
-let value2 = 7;
+// let value1 = 6;
+// let value2 = 7;
 
-//add value2 with value1 and store the result inside value1
+// //add value2 with value1 and store the result inside value1
 
-console.log(value1+" before the operation")
+// console.log(value1+" before the operation")
 
-value1 += value2; //value1 = value1 + value2
+// value1 += value2; //value1 = value1 + value2
 
-console.log(value1+" after the operation")
+// console.log(value1+" after the operation");
+
+// let a = "5";
+// let b =  5;
+// console.log(a > b); //  false
+// console.log(a < b); // false
+// console.log(a == b) //true
+// console.log(a != b); //false
+// console.log(a >= b); //true
+// console.log(a <= b ); // true
+// console.log(a === b); //false
+// console.log(a !== b);// true
+
+// let databaseUsername = "tony@gmail.com";
+// let databaseUserPassword = "Tony@1234";
+
+// let username = "tony@gmail.com";
+// let password = "Tony@1234";
+
+// console.log(databaseUsername == username && databaseUserPassword == password);
+
+// // console.log(!false);
+
+
+let a = 6;
+
+console.log(a++ );  //6
+console.log(a ++ ); //7
+console.log(a ++);  //8
+console.log(++ a);  //10
+console.log(-- a);  //9
+console.log(++ a);  //10
+console.log(a ++);  //10
+console.log(a --);  //11
+console.log(a --);  //10
+console.log(-- a);  //8
+console.log(a++ );  //8
+console.log(a);     //9
